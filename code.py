5f53f5354973f310493c6825fb3e4ef31232aa2a
Amit Joseph <amitbj96@yahoo.com>
2019-09-04 18:22:32 +0530

    Fuctions added :fire:

--- code.py
+++ code.py
@@ -1,1 +1,24 @@
-print("Hello")
+import urllib,json
+
+def UpdateIp2Dyn(ip):
+    updateURL="http://USERNAME:PASSWORD@members.dyndns.org/nic/update?hostname=YOURDOMAIN.dlinkddns.com&myip="+ip+"&wildcard=NOCHG&mx=NOCHG&backmx=NOCHG"
+    try:
+        response =urllib.urlopen(updateURL)
+        print response.read()
+    except IOError:
+            pass
+    
+
+
+def getPublicIP():
+    publicIpURL="https://api.ipify.org?format=json"
+    try:
+        response =urllib.urlopen(publicIpURL)
+        data=json.loads(response.read())
+        ip=data['ip']
+        return ip
+    except IOError:
+        print 'No Internet!'
+    
+ip=str(getPublicIP())
+UpdateIp2Dyn(ip)
