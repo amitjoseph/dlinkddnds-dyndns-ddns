4bd986a25c59d9a40b1b8a4728c295b44c432910
Amit Joseph <amitbj96@yahoo.com>
2019-09-11 13:57:29 +0530

    added logging

--- code.py
+++ code.py
@@ -1,10 +1,13 @@
-import urllib,json,socket
+import urllib,json,socket,logging
 
 # TO CHANGE --------------------
 username="YOUR_USERNAME"
 password="YOUR_PASSWORD"
 domain="DOMAINNAME"
 # ------------------------------
+
+#Logging Configuration
+logging.basicConfig(filename="ddns.log", format='%(asctime)s %(message)s',level=logging.DEBUG) 
 
 def updateIp2Dyn(ip):
     updateURL="http://"+username+":"+password+"@members.dyndns.org/nic/update?hostname="+domain+"&myip="+ip+"&wildcard=NOCHG&mx=NOCHG&backmx=NOCHG"
@@ -15,7 +18,7 @@ def updateIp2Dyn(ip):
             pass
     
 
-
+#Get the public IP from ipify
 def getPublicIP():
     publicIpURL="https://api.ipify.org?format=json"
     try:
@@ -26,7 +29,8 @@ def getPublicIP():
     except IOError:
         print 'No Internet!'
     
-
+# Checks IP of the Domain and Public
+# Updates new IP if it has changed
 def checkDns():
     dnsIP=str(socket.gethostbyname(domain))
     publicIP=getPublicIP()
